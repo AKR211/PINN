7e3b008558eb7cd89848d3c28a50f548196d54dd
AKR211 <agneyk.rajeev@niser.ac.in>
2023-10-16 21:54:32 +0530

    commented both codes for better readability

--- harmonic_oscillator.py
+++ harmonic_oscillator.py
@@ -1,15 +1,11 @@
 import numpy as np
-import torch
 from torch import tensor,zeros_like,FloatTensor,linspace,manual_seed,ones_like
 from torch.autograd import grad
 from torch.optim import Adam
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
-def truef(x):
-	cos = torch.sin(20.0*x)
-	return cos
-
+#Define the architecture of the neural network
 class NeuralNet(nn.Module):
 	def __init__(self,numinputs=1,numlayers=3,numoutputs=1,numneurons=8):
 		super().__init__()
@@ -21,7 +17,7 @@ class NeuralNet(nn.Module):
 		layers=[]
 		layers.append(nn.Linear(self.ni,self.nneu))
 		
-		for _ in range(self.nl-1):
+		for _ in range(self.nl):
 			layers.append(nn.Linear(self.nneu,self.nneu))   #create each layer
 			layers.append(nn.Tanh())  #tanh activation function is ised
 		
@@ -32,16 +28,17 @@ class NeuralNet(nn.Module):
 	def forward(self,input):
 		return self.Network(input.view(-1,1))    #forward propogator
 
+#the derivative calculator for nth degree
 def dnfdxn(n,model,x_values):
 	out = model(x_values)
 	for i in range(n):
 		out = grad(out, x_values, ones_like(out), create_graph=True)[0]
 	return out.view(-1,1)
 
+#NOW WE SOLVE THE HARMONIC OSCILLATOR DIFFERENTIAL EQUATION
+
 R = 20.0
-x = torch.linspace(0,1,500).view(-1,1)
-y = truef(x).view(-1,1)
-x_data = tensor([0.,0.1,0.2]).view(-1,1)
+x_data = tensor([0.,0.1,0.2]).view(-1,1) #sample points
 y_data = tensor([0.,0.9093,-0.7568]).view(-1,1)
 
 #creates the loss function using the necessary derivatives
@@ -51,11 +48,11 @@ def loss_func_maker(f,dnfdxn):
 	def loss_func(x):
 	
 		f_value = f(x)	
-		DEloss = dnfdxn(2,f,x) + (R**2)*f_value   #loss due to differetnial equation constraint
+		DEloss = dnfdxn(2,f,x) + (R**2)*f_value   #loss due to differential equation constraint
 
 		x0 = x_data
 		f0 = y_data
-		Bdryloss = f(x0) - f0 #loss due to the boundary constraint
+		Bdryloss = f(x0) - f0 #loss due to the sample points
 
 		loss = nn.MSELoss()
 		loss_val = (1e-4)*loss(DEloss,zeros_like(DEloss)) + loss(Bdryloss,zeros_like(Bdryloss))
@@ -63,24 +60,51 @@ def loss_func_maker(f,dnfdxn):
 
 	return loss_func
 
-x_physics = torch.linspace(0,1,30).view(-1,1).requires_grad_(True)
+#for recreating the same output
+manual_seed(123)
 
-torch.manual_seed(123)
-model = NeuralNet(1,3,1,32)
-optimizer = torch.optim.Adam(model.parameters(),lr=1e-4)
-loss_func = loss_func_maker(model,dnfdxn)
-#x_physics = FloatTensor(30).uniform_(0,1).view(-1,1).requires_grad_(True)
-x_physics = torch.linspace(0,1,30).view(-1,1).requires_grad_(True)
-for i in range(30000):
+#define all specifications
+inputs=1
+outputs=1
+layers=2
+neurons=32
+learning_rate=1e-4
+num_epochs=20000
+batch_size=30
+domain=(0.,1.0)
+
+#we use the adam optimizer
+
+f = NeuralNet(inputs,layers,outputs,neurons)
+optimizer = Adam(f.parameters(),lr=learning_rate)
+loss_func = loss_func_maker(f,dnfdxn)
+
+x = linspace(domain[0],domain[1],batch_size).view(-1,1).requires_grad_(True)
+losses=[]
+for i in range(num_epochs):
     optimizer.zero_grad()
-    loss = loss_func(x_physics)
+    loss = loss_func(x)                         #loss for the batch
     loss.backward()
-    optimizer.step()
+    optimizer.step()                            #optimization step
+    losses.append(float(loss))
 
-X = torch.linspace(0,1,500).reshape(-1,1)
-Y = model(X)
-plt.plot(x,y.detach().numpy())
+X = linspace(domain[0],domain[1],500).reshape(-1,1)   #test data
+Y = f(X)
+truef = lambda x: np.sin(20.0*x)         #analytical solution
+Y_ = truef(X).view(-1,1)
+
+plt.figure(figsize=(11.8,4.8))
+#plot the neural network solution vs the analytical solution
+plt.subplot(1,2,1)
 plt.plot(X.detach().numpy(),Y.detach().numpy())
-plt.scatter(x_data,y_data)
-print(x_data,y_data)
+plt.plot(X.detach().numpy(),Y_.detach().numpy())
+plt.scatter(x_data.detach().numpy(),y_data.detach().numpy())
+plt.xlabel('x')
+plt.ylabel('f(x)')
+
+#plot the loss with each epoch
+plt.subplot(1,2,2)
+plt.plot(range(1,num_epochs+1),losses)
+plt.xlabel('epoch')
+plt.ylabel('loss')
 plt.show()
--- logistic_DE.py
+++ logistic_DE.py
@@ -19,7 +19,7 @@ class NeuralNet(nn.Module):
 		
 		for _ in range(self.nl):
 			layers.append(nn.Linear(self.nneu,self.nneu))   #create each layer
-			layers.append(nn.Tanh())  #tanh activation function is ised
+			layers.append(nn.Tanh())  #tanh activation function is used
 		
 		layers.append(nn.Linear(self.nneu,self.no))
 
@@ -28,6 +28,7 @@ class NeuralNet(nn.Module):
 	def forward(self,input):
 		return self.Network(input.view(-1,1))    #forward propogator
 
+#the derivative calculator for nth degree
 def dnfdxn(n,model,x_values):
 	out = model(x_values)
 	for i in range(n):
@@ -36,23 +37,22 @@ def dnfdxn(n,model,x_values):
 
 #NOW WE SOLVE THE LOGISTIC DIFFERENTIAL EQUATION
 
-
 R = 1.0
-x_boundary = 0.0
-f_boundary = 0.5
+x_data = tensor([0.0]) #sample points
+y_data = tensor([0.5])
 
 #creates the loss function using the necessary derivatives
 def loss_func_maker(f,dnfdxn):
 	
-	#The loss function is a sum of MSE loss due to the differetnial equation constraint and the boundary constraints
+	#The loss function is a sum of MSE loss due to the differential equation constraint and the sample points
 	def loss_func(x):
 	
 		f_value = f(x)	
 		DEloss = dnfdxn(1,f,x) - R*f_value*(1-f_value)   #loss due to differetnial equation constraint
 
-		x0 = x_boundary
-		f0 = f_boundary
-		Bdryloss = f(tensor([x0])) - tensor([f0]) #loss due to the boundary constraint
+		x0 = x_data
+		f0 = y_data
+		Bdryloss = f(x0) - f0 #loss due to the sample points
 
 		loss = nn.MSELoss()
 		loss_val = loss(DEloss,zeros_like(DEloss)) + loss(Bdryloss,zeros_like(Bdryloss))
@@ -73,25 +73,24 @@ num_epochs=10000
 batch_size=30
 domain=(-5.0,5.0)
 
-#we use the adam optimizer of torchopt module for functional API
-
+#we use the adam optimizer
 
 f = NeuralNet(inputs,layers,outputs,neurons)
 optimizer = Adam(f.parameters(),lr=learning_rate)
 loss_func = loss_func_maker(f,dnfdxn)
 
-x = linspace(domain[0],domain[1],batch_size).view(-1,1).requires_grad_(True)	       #training data for 1 batch
+x = linspace(domain[0],domain[1],batch_size).view(-1,1).requires_grad_(True)	       #training data points for DE
 losses=[]
 for i in range(num_epochs):
 	optimizer.zero_grad()
-	loss = loss_func(x)                                      #loss for the batch
+	loss = loss_func(x)                         #loss for the batch
 	loss.backward()
 	optimizer.step()                            #optimization step
 	losses.append(float(loss))
 
-X=linspace(domain[0],domain[1],100).reshape(-1,1)                   #test data
+X=linspace(domain[0],domain[1],100).reshape(-1,1)   #test data
 Y=f(X)
-truef=lambda x: 1.0/(1.0+(1.0/f_boundary-1.0)*np.exp(-R*x))         #analytical solution
+truef=lambda x: 1/(1 + np.exp(-R*x))         #analytical solution
 Y_=truef(X)
 
 plt.figure(figsize=(11.8,4.8))
@@ -99,6 +98,7 @@ plt.figure(figsize=(11.8,4.8))
 plt.subplot(1,2,1)
 plt.plot(X.detach().numpy(),Y.detach().numpy())
 plt.plot(X.detach().numpy(),Y_.detach().numpy())
+plt.scatter(x_data.detach().numpy(),y_data.detach().numpy())
 plt.xlabel('x')
 plt.ylabel('f(x)')
 
